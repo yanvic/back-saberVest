345e15c8d9216d27cd7fc1dccb0bd9c49b2746bc
yanvic <yanvictorce@gmail.com>
2024-10-27 15:56:54 -0300

    testando filtros

--- internal/handlers/questions.go
+++ internal/handlers/questions.go
@@ -29,23 +29,45 @@ func GetQuestions(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetQuestionsParams(w http.ResponseWriter, r *http.Request) {
-	topic := r.URL.Query().Get("topic")
-	matter := r.URL.Query().Get("matter")
+	topicId := r.URL.Query().Get("topic")
+	matterId := r.URL.Query().Get("matter")
+	universityId := r.URL.Query().Get("university")
 	dateInitial := r.URL.Query().Get("date_initial")
 	dateFinal := r.URL.Query().Get("date_final")
 
 	pageStr := r.URL.Query().Get("page")
 
+	//page, err := strconv.Atoi(pageStr)
+	//if err != nil {
+	//	http.Error(w, "error is convert", http.StatusBadRequest)
+	//	return
+	//}
 	page, err := strconv.Atoi(pageStr)
-	if err != nil {
-		http.Error(w, "error is convert", http.StatusBadRequest)
-		return
+	if err != nil || page <= 0 {
+		log.Printf("Parâmetro de página inválido: %v, usando 1 como padrão", pageStr)
+		page = 1
 	}
 
-	offset := page
-	limit := offset - page
+	topic, err := strconv.Atoi(topicId)
+	if err != nil || topic <= 0 {
+		log.Printf("Parâmetro de página inválido: %v, usando 1 como padrão", topicId)
+		page = 1
+	}
+	matter, err := strconv.Atoi(matterId)
+	if err != nil || matter <= 0 {
+		log.Printf("Parâmetro de página inválido: %v, usando 1 como padrão", matterId)
+		page = 1
+	}
+	university, err := strconv.Atoi(universityId)
+	if err != nil || matter <= 0 {
+		log.Printf("Parâmetro de página inválido: %v, usando 1 como padrão", universityId)
+		page = 1
+	}
 
-	if topic == "" || matter == "" || dateInitial == "" {
+	offset := page - 1
+	limit := 10
+
+	if topic == 0 || dateInitial == "" || dateFinal == "" || university == 0 {
 		http.Error(w, "Params is required", http.StatusBadRequest)
 		return
 	}
@@ -53,6 +75,7 @@ func GetQuestionsParams(w http.ResponseWriter, r *http.Request) {
 	filterQuestions := models.FilterQuestions{
 		Topic:       topic,
 		Matter:      matter,
+		University:  university,
 		InitialDate: dateInitial,
 		FinalDate:   dateFinal,
 		Limit:       limit,
--- internal/models/question.go
+++ internal/models/question.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+	"github.com/lib/pq"
+	"time"
+)
+
 type Matter struct {
 	Id          int `json:"id"`
 	Description int `json:"description"`
@@ -27,22 +33,27 @@ type Response struct {
 }
 
 type Test struct {
-	Id          int     `json:"id"`
-	Description *string `json:"description"`
-	Number      int     `json:"number"`
-	Question    string  `json:"question"`
-	Alternative string  `json:"alternative"`
-	Response    int     `json:"response"`
-	ResponseId  string  `json:"response_id"`
-	TextId      *int    `json:"text_id"`
-	ImageId     *int    `json:"image_id"`
-	TopicId     int     `json:"topic_id"`
-	NoteId      *int    `json:"note_id"`
+	Id           int       `json:"id"`
+	Description  *string   `json:"description"`
+	Number       *int      `json:"number"`
+	Question     *string   `json:"question"`
+	Alternative  *string   `json:"alternative"`
+	Response     *string   `json:"response"`
+	ResponseId   *int      `json:"response_id"`
+	TextId       *int      `json:"text_id"`
+	ImageId      *int      `json:"image_id"`
+	TopicId      *int      `json:"topic_id"`
+	NoteId       *int      `json:"note_id"`
+	Alternatives []string  `json:"alternatives"`
+	Date         time.Time `json:"date"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 type FilterQuestions struct {
-	Topic       string `json:"topic"`
-	Matter      string `json:"matter"`
+	Topic       int    `json:"topic"`
+	University  int    `json:"university"`
+	Matter      int    `json:"matter"`
 	InitialDate string `json:"date_initial"`
 	FinalDate   string `json:"final_date"`
 	Offset      int    `json:"offset"`
@@ -61,7 +72,8 @@ func (c *Test) AllQuestions() ([]Test, error) {
 	for rows.Next() {
 		var test Test
 		err := rows.Scan(&test.Id, &test.Description, &test.Number, &test.Question, &test.Alternative,
-			&test.Response, &test.TextId, &test.ImageId, &test.TopicId, &test.NoteId, &test.ResponseId)
+			&test.Response, &test.TextId, &test.ImageId, &test.TopicId, &test.NoteId, &test.ResponseId,
+			pq.Array(&test.Alternatives), &test.Date, &test.CreatedAt, &test.UpdatedAt)
 		if err != nil {
 			return nil, err
 		}
@@ -75,14 +87,57 @@ func (c *Test) AllQuestions() ([]Test, error) {
 }
 
 func (c *Test) QuestionsParams(filters FilterQuestions) ([]Test, error) {
-	rows, err := DB.Query(`SELECT t.id, t.description, t.number, t.statement, t.alternative, t.response_id, t.text_id, t.image_id, 
-       t.topic_id, t.note_id, t.response, tx.description, img.image, nt.description
-	   FROM public.test_uece
-	   LEFT JOIN image img ON (img.id = t.image_id)
-	   LEFT JOIN text tx ON (tx.id = t.text_id)
-	   LEFT JOIN note nt ON (nt.id = t.note_id)
-	   `)
+	query := (`SELECT 
+		t.id, t.description, t.number, t.statement AS question, 
+		t.alternative, t.response, t.response_id, 
+		t.text_id, t.image_id, t.topic_id, t.note_id, 
+		t.alternatives, t.date, t.created_at, t.updated_at
+FROM 
+    public.test_uece t
+LEFT JOIN 
+    image img ON img.id = t.image_id
+LEFT JOIN 
+    text tx ON tx.id = t.text_id
+LEFT JOIN 
+    note nt ON nt.id = t.note_id
+WHERE 
+`)
 
+	var params []interface{}
+	var paramCount int
+
+	if filters.University != 0 {
+		paramCount++
+		query += fmt.Sprintf(" AND t.university_id = $%d", paramCount)
+		params = append(params, filters.Topic)
+	}
+
+	if filters.Topic != 0 {
+		paramCount++
+		query += fmt.Sprintf(" AND t.topic_id = $%d", paramCount)
+		params = append(params, filters.Topic)
+	}
+
+	if filters.InitialDate != "" && filters.FinalDate != "" {
+		initialYear := filters.InitialDate[:4]
+		finalYear := filters.FinalDate[:4]
+
+		query += fmt.Sprintf(" AND t.date BETWEEN '%s-01-01' AND '%s-12-31'", initialYear, finalYear)
+	}
+
+	if filters.Offset >= 0 {
+		paramCount++
+		query += fmt.Sprintf(" OFFSET $%d", paramCount)
+		params = append(params, filters.Offset)
+	}
+
+	if filters.Limit > 0 {
+		paramCount++
+		query += fmt.Sprintf(" LIMIT $%d", paramCount)
+		params = append(params, filters.Limit)
+	}
+
+	rows, err := DB.Query(query, params...)
 	if err != nil {
 		return nil, err
 	}
@@ -92,8 +147,12 @@ func (c *Test) QuestionsParams(filters FilterQuestions) ([]Test, error) {
 
 	for rows.Next() {
 		var test Test
-		err := rows.Scan(&test.Id, &test.Description, &test.Number, &test.Question, &test.Alternative,
-			&test.Response, &test.TextId, &test.ImageId, &test.TopicId, &test.NoteId, &test.ResponseId)
+		err := rows.Scan(
+			&test.Id, &test.Description, &test.Number, &test.Question,
+			&test.Alternative, &test.Response, &test.ResponseId,
+			&test.TextId, &test.ImageId, &test.TopicId, &test.NoteId,
+			pq.Array(&test.Alternatives), &test.Date, &test.CreatedAt, &test.UpdatedAt,
+		)
 		if err != nil {
 			return nil, err
 		}
